5c266cb78231e5ef3797d913256ea37f96ed8f31
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-21 17:53:15 -0300

    Adiciona envio de mensagem para a fila via Camel

--- src/br/com/caelum/livraria/camel/ConfiguracaoCamel.java
+++ src/br/com/caelum/livraria/camel/ConfiguracaoCamel.java
@@ -3,20 +3,36 @@ package br.com.caelum.livraria.camel;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConfiguracaoCamel {
 
-
+	@Autowired
+	private CamelContext context;
+	
 	@PostConstruct
 	void init() throws Exception {
-
+		context.addRoutes(new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+				from("jms:topic:jms/TOPIC.LIVRARIA?username=jms&password=jms2")
+					.log(LoggingLevel.INFO, "Camel: Recebendo mensagem ${id}")
+						.to("jms:queue:jms/FILA.GERADOR?username=jms&password=jms2");
+			}
+		});
+		
+		context.start();
 	}
 	
 	@PreDestroy
 	void destroy() throws Exception {
-
+		context.stop();
 	}
 
 }
